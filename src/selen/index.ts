b355d26cea321a94f2ffad4fad3cf695cb6a910d
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-01-22 18:08:17 +0900

    Selen.Build() and Selen.setPath() are depreciated (#50)

--- src/selen/index.ts
+++ src/selen/index.ts
@@ -5,17 +5,19 @@ export { Pages, SelenOptions, SelenOptions as PageOptions, WebElementEx, Diction
 export class Selen {
   public static Pages = Pages;
   public static setPath(path: string){
+    console.warn("Selen.setPath() is depreciated. Use Selen.addPath() instead.");
+    Selen.addPath(path);
+  }
+  public static addPath(path: string){
     const sep = process.platform == "win32" ? ";" : ":";
     process.env.PATH += `${sep}${path}`;
   }
-  public static addPath(path: string){
-    Selen.setPath(path);
-  }
   public static async Build(browser: string){
-    return new Builder().forBrowser(browser).build();
+    console.warn("Selen.Build() is depreciated. Use Selen.build() instead.");
+    return this.build(browser);
   }
   public static async build(browser: string){
-    return this.Build(browser);
+    return new Builder().forBrowser(browser).build();
   }
 }
 
